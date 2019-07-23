4ec61794de1b3d078088b4e61c95162d41040d6b
aditya <adityakmr9672@gmail.com>
2019-07-23 16:48:15 +0530

    clean code

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-//controller imports
+import auth from './auth';
+//import controllers
 import basicController from './controllers/basicController';
 import userController from './controllers/userController';
 import postController from './controllers/postController';
@@ -13,11 +14,13 @@ routes.post('/signup', userController.post);
 routes.post('/login', userController.login);
 
 //Post Rotues
+//TODO: add authentication routes
+
 routes.post('/post', postController.post);
 routes.get('/posts', postController.getAll);
 
 //comments routes
-routes.post('/comment', commentController.post);
+routes.post('/comment', auth.required, commentController.post);
 
 
 export default routes;
